ac6c18bcf98f10a9f215ad5a92a434586b55ec6e
Rudy Eko Prasetya <rudyekoprasetya@yahoo.co.id>
2022-11-21 17:48:32 +0700

    [EDIT] App JS

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [Count,setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Nama Saya Rudy Eko Prasetya</h1>
-        <p>
-         Selamat Belajar React JS
-        </p>
+         <h1>Nama Saya Rudy Eko Prasetya</h1>
+          <p>
+           Selamat Belajar React JS
+          </p> 
+        <p>Anda Klik Sebanyak {Count} kali</p>
+        <button onClick={() => setCount(Count + 1)}>Klik Disini</button>
       </header>
     </div>
   );
